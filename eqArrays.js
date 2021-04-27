42584fed1d2dfc5cb46f4fd5e5b81b8754185f05
Zobee <zohebboga@hotmail.com>
2021-04-27 18:17:57 +0000

    Created a brand spankin' new eqArrays function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if(arr1.length !== arr2.length){
+    return false
+  }
+  for(let i = 0; i < arr1.length; i++){
+    let valA = arr1[i]
+    let valB = arr2[i]
+    if (valA !== valB){
+      return false
+    }
+  }
+  return true
+}
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), false);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)

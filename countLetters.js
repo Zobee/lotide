a2dd05e4241ff3330bde201e06fa94a54a1c0c00
Zobee <zohebboga@hotmail.com>
2021-04-28 18:29:03 +0000

    Added new functions for working with and iterating through objects

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(str){
+  const countObj = {}
+
+  for(let char of str){
+    if(char !== " "){
+      if(countObj[char]){
+        countObj[char]++
+      } else {
+        countObj[char] = 1
+      }
+    }
+  }
+  return countObj
+}
+
+console.log(countLetters("lighthouse in the house"))
---
+++ countOnly.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const countObj = {}
+
+  for(let item of allItems){
+    if(itemsToCount[item]){
+      if(countObj[item]){
+        countObj[item]++
+      } else {
+        countObj[item] = 1
+      }
+    }
+  }
+
+  return countObj
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);

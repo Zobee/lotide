ea892046e33f8644668ac7fedfe79340e4c3da2e
Zobee <zohebboga@hotmail.com>
2021-04-28 18:44:18 +0000

    Created findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (obj, val) => {
+  for(let key in obj){
+    if (obj[key] === val){
+      return key
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const favFoods = {
+  fruit: "banana",
+  main: "steak",
+  side: "sweet potato fries",
+  drink: "moscow mule"
+}
+
+assertEqual(findKeyByValue(favFoods, "sweet potato fries"), "side");
+assertEqual(findKeyByValue(favFoods, "moscow mule"), "drink");
+assertEqual(findKeyByValue(favFoods, "cheesecake"), undefined);

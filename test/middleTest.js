c10b7112fd00eac6a9c23647b4e2636dcee7e071
Zobee <zohebboga@hotmail.com>
2021-05-03 19:31:31 +0000

    Refactored tailTest and middleTest to utilize mocha+chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,24 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle(["a", "b", "c", 'd']), ['b', 'c']);
-assertArraysEqual(middle([1, true, "tree", 4, false, 6, "heaven"]), [4]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8,9,10,11,12]), [6, 7]);
+describe("#middle", () => {
+  it("Should return an empty array if input length < 3", () => {
+    assert.deepEqual(middle([1]), [])
+  })
+  it("Should return a single element if input length is odd", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3])
+  })
+  it("Should return two elements if input length is even", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  })
+  it("Should work with different typed values", () => {
+    assert.deepEqual(middle([1, true, "tree", 4, false, 6, "heaven"]), [4]);
+  })
+  it("Should work with a very large input", () => {
+    let arr = []
+    for(let i = 1; i < 10000; i++){
+      arr.push(i);
+    }
+    assert.deepEqual(middle(arr), [5000])
+  })
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,22 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require("../assertEqual");
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("Should not modify the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.deepEqual(words, ["Yo Yo", "Lighthouse", "Labs"])
+  });
 
-// Test Case: Check each value in the array
-const nums = [1,2,3,4,5];
-let numTail = tail(nums);
+  it("Should return every value in the array minus the first element", () => {
+    const nums = [1,2,3,4,5];
+    let numTail = tail(nums);
 
-for (let i = 0; i < numTail.length; i++) {
-  assertEqual(numTail[i], nums[i + 1]);
-}
+    assert.deepEqual(numTail, [2,3,4,5])
+  });
+
+  it("Should return an empty array if input array's length is <= 1", () => {
+    const one = [1];
+    assert.deepEqual(tail(one), [])
+  })
+})
